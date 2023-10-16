cc8848cebd6b99633750be7a8993865f44720a8f
hkir-dev <huseyin_kir@yahoo.com>
2023-10-16 10:37:12 +0100

    first commit

--- src/hashing.py
+++ src/hashing.py
@@ -16,6 +16,8 @@ print("CRC32 Hex str: {}".format('%x' % crc32_hash))
 
 print("Zlib adler32: {}".format(zlib.adler32(text_bytes)))
 
+print("Zlib adler32 hex: {}".format('%x' % zlib.adler32(text_bytes)))
+
 
 # OUT:
 # binascii CRC32: 2510416923
@@ -23,3 +25,4 @@ print("Zlib adler32: {}".format(zlib.adler32(text_bytes)))
 # CRC32 Hex: 0x95a1ec1b
 # CRC32 Hex str: 95a1ec1b
 # Zlib adler32: 2546535024
+# Zlib adler32 hex: 97c90a70
