5cbf89c03540ee7690c598fd0801acdf10d13cac
hkir-dev <huseyin_kir@yahoo.com>
2023-11-01 14:55:05 +0000

    blake2b experiment

--- src/hashing.py
+++ src/hashing.py
@@ -1,5 +1,8 @@
 import binascii
 import zlib
+import uuid
+import base64
+import hashlib
 
 text = "akhjsdfjl, akuendc, hiuahhas"
 text_bytes = str.encode(text)
@@ -18,6 +21,24 @@ print("Zlib adler32: {}".format(zlib.adler32(text_bytes)))
 
 print("Zlib adler32 hex: {}".format('%x' % zlib.adler32(text_bytes)))
 
+uuid = str(uuid.uuid3(uuid.NAMESPACE_URL, text))
+
+print("UUID: {}".format(uuid))
+
+print("UUID base64: {}".format(base64.b64encode(str.encode(uuid))))
+
+print("##### 64 bit hashing algorithms ######")
+
+blake_hasher = hashlib.blake2b(text_bytes, digest_size=5)
+print("Blake2b bytes: {}".format(blake_hasher.digest()))
+
+blake_hex = blake_hasher.hexdigest()
+print("Blake2b Hex: {}  ({})".format(blake_hex, len(blake_hex)))
+
+blake_base64 = str(base64.urlsafe_b64encode(blake_hasher.digest()), 'UTF-8')
+print("Blake2b base64: {} ({})".format(blake_base64, len(blake_base64)))
+
+
 
 # OUT:
 # binascii CRC32: 2510416923
@@ -26,3 +47,8 @@ print("Zlib adler32 hex: {}".format('%x' % zlib.adler32(text_bytes)))
 # CRC32 Hex str: 95a1ec1b
 # Zlib adler32: 2546535024
 # Zlib adler32 hex: 97c90a70
+
+
+
+
+
--- src/test/collusion_test.py
+++ src/test/collusion_test.py
@@ -4,12 +4,14 @@ import random
 import zlib
 import binascii
 import datetime
+import hashlib
+import base64
 
 
 class HashingTests(unittest.TestCase):
     def test_collusion(self):
         barcodes = list()
-        for i in range(100000):
+        for i in range(100000000):
             barcodes.append(''.join(random.choices(string.ascii_uppercase + string.digits, k=100)))
 
         print(barcodes[0])
@@ -18,15 +20,36 @@ class HashingTests(unittest.TestCase):
         print(barcodes[3])
 
         hashes = set()
+        lens = set()
         conflict_count = 0
         for barcode in barcodes:
-            bhash = zlib.crc32(str.encode(barcode))
+            bhash = '%x' % zlib.crc32(str.encode(barcode))
             if bhash in hashes:
                 conflict_count += 1
             else:
                 hashes.add(bhash)
+            if len(bhash) not in lens:
+                lens.add(len(bhash))
 
-        print("Conflict count: {}".format(conflict_count))
+        print("Conflict count crc32: {}".format(conflict_count))
+        print("Hash lengths: {}".format(lens))
+
+        hashes = set()
+        lens = set()
+        conflict_count = 0
+        for barcode in barcodes:
+            blake_hasher = hashlib.blake2b(str.encode(barcode), digest_size=6)
+            # bhash = base64.urlsafe_b64encode(blake_hasher.digest())
+            bhash = blake_hasher.hexdigest()
+            if bhash in hashes:
+                conflict_count += 1
+            else:
+                hashes.add(bhash)
+            if len(bhash) not in lens:
+                lens.add(len(bhash))
+
+        print("Conflict count blake2b: {}".format(conflict_count))
+        print("Hash lengths: {}".format(lens))
 
     def test_lib_performance(self):
         barcodes = list()
@@ -48,4 +71,12 @@ class HashingTests(unittest.TestCase):
         duration = end - start
 
         print("binascii: {}".format(duration.microseconds))
+
+        start = datetime.datetime.now()
+        for barcode in barcodes:
+            hashlib.blake2b(str.encode(barcode), digest_size=5).digest()
+        end = datetime.datetime.now()
+        duration = end - start
+
+        print("blake2b: {}".format(duration.microseconds))
 
